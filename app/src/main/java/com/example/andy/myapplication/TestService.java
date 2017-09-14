dffeaa051760b5815bf42b4ba73bd97e2505b84b
fanyunhui <fan@example.com>
2017-09-14 20:14:10 +0800

    create order branch

--- app/src/main/java/com/example/andy/myapplication/TestService.java
+++ app/src/main/java/com/example/andy/myapplication/TestService.java
@@ -18,9 +18,7 @@ public class TestService extends Service {
 
     public class MyBind extends Binder{
         public void callTest(String str){
-
-
-
+            //test tags
             test(str);
         }
     }
